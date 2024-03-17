2b2abbfcdb0928adde7bf9554ad680d61f556e73
David Rivero <davidmiguel22573@gmail.com>
2024-03-16 18:25:16 -0700

    tutorial checkpoint - database start

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'www.redgifs.com',
+            port: '',
+            pathname: '/**',
+          },
+        ],
+      },
+};
 
 module.exports = nextConfig;
